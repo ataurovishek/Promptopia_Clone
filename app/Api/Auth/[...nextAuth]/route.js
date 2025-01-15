d96862615bcc5c94f0d9f58c51383972fbfc4655
ataur rahman <ataurovishek@gmail.com>
2025-01-15 21:36:51 +0600

    build session

--- app/Api/Auth/[...nextAuth]/route.js
+++ app/Api/Auth/[...nextAuth]/route.js
@@ -1,34 +1,49 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectDB } from "@utils/database";
-
+import { User } from "@model/user";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
-    ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 
-    async session({ session }) {
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
 
-    },
-    async signIn({ profile }) {
-        try {
-            await connectDB()
-
-            // check if a user exists in the database
+    session.user.id = sessionUser._id.toString();
 
 
-            // if not create a new user 
-            return true
-        } catch (error) {
-            console.log(error);
-            return false
+    return sessionUser;
+  },
 
-        }
+  async signIn({ profile }) {
+    try {
+      await connectDB();
+
+      // check if a user exists in the database
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+      // if not create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.username.trim().toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-})
+  },
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- model/user.js
+++ model/user.js
@@ -1,1 +1,30 @@
-import { Schema,models,model } from "mongoose";
+import { Schema, models, model } from "mongoose";
+
+
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, "Email is already taken"],
+        required: [true, "Email is Required"],
+    },
+    username: {
+        type: String,
+        unique: [true, "Username is already taken"],
+        required: [true, "username is required"],
+        match:[/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid,it should contain 8-20 alphanumeric characters and be unique"]
+    },
+    image: {
+        type: String,
+    }
+})
+
+export const User = models.User || model("User",userSchema)
+
+// the models object is provided by the mongoose library and stores all the regstered modelss.
+
+//if a model named "User" is already registered,it will  assign the existing model to the "User" variable.
+
+// this prevents redefined the model and ensure that the existing model is reused
+
+//if a model named "User" doesnot exist in the "models" object the "model" function from mongoose is called to create a new model.
